01c9267beb0ed2a24ff0304203cfe01af82d3445
7yl4r <murray.tylar@gmail.com>
2019-01-08 22:01:06 -0500

    reformatting

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
-
 cytoscape({
   container: document.getElementById('cy'),
-
   style: cytoscape.stylesheet()
     .selector('node')
       .css({
@@ -37,32 +35,29 @@ cytoscape({
         'text-opacity': 0
       }),
 
-  elements: {
-    nodes: [
-      { data: { id: 'a', foo: 3, bar: 5, baz: 2 } },
-      { data: { id: 'b', foo: 6, bar: 1, baz: 3 } },
-      { data: { id: 'c', foo: 2, bar: 3, baz: 5 } },
-      { data: { id: 'd', foo: 7, bar: 1, baz: 2 } },
-      { data: { id: 'e', foo: 2, bar: 3, baz: 5 } }
-    ],
-
-    edges: [
-      { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
-      { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
-      { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
-      { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
-      { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
-      { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
-      { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
-    ]
-  },
-
-  layout: {
-    name: 'circle',
-    padding: 10
-  },
-
-  ready: function(){
-    window.cy = this;
-  }
+    elements: {
+        nodes: [
+            { data: { id: 'a', foo: 3, bar: 5, baz: 2 } },
+            { data: { id: 'b', foo: 6, bar: 1, baz: 3 } },
+            { data: { id: 'c', foo: 2, bar: 3, baz: 5 } },
+            { data: { id: 'd', foo: 7, bar: 1, baz: 2 } },
+            { data: { id: 'e', foo: 2, bar: 3, baz: 5 } }
+        ],
+        edges: [
+            { data: { id: 'ae', weight: 1, source: 'a', target: 'e' } },
+            { data: { id: 'ab', weight: 3, source: 'a', target: 'b' } },
+            { data: { id: 'be', weight: 4, source: 'b', target: 'e' } },
+            { data: { id: 'bc', weight: 5, source: 'b', target: 'c' } },
+            { data: { id: 'ce', weight: 6, source: 'c', target: 'e' } },
+            { data: { id: 'cd', weight: 2, source: 'c', target: 'd' } },
+            { data: { id: 'de', weight: 7, source: 'd', target: 'e' } }
+        ]
+    },
+    layout: {
+        name: 'circle',
+        padding: 10
+    },
+    ready: function(){
+        window.cy = this;
+    }
 });
